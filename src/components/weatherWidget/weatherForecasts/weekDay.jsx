0364a088e10adedf2d9204081bee8fcefdd78362
Crepkey <crepkey@gmail.com>
2019-11-15 10:11:07 +0100

    Forecasts comp calculates which day is today

--- src/components/weatherWidget/weatherForecasts/weekDay.jsx
+++ src/components/weatherWidget/weatherForecasts/weekDay.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import moment from "moment";
 
 const WeekDay = props => {
-  return <h3>{moment(props.date).format("dddd")}</h3>;
+return (
+    <h3>
+      {moment().format("YYYY-MM-DD") === moment(props.date).format("YYYY-MM-DD")
+        ? "Today"
+        : moment(props.date).format("dddd")}
+    </h3>
+  );
 };
 
 export default WeekDay;
