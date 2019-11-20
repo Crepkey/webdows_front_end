0f500a84c5c770b93233de29d2079de179c57c2f
Crepkey <crepkey@gmail.com>
2019-11-20 20:47:21 +0100

    Loader comp is handling any style settings

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -3,23 +3,32 @@ import { ReactComponent as Loader1 } from "../svg/loader_1.svg";
 import { ReactComponent as Loader2 } from "../svg/loader_2.svg";
 
 const Loader = props => {
-  if (props.data === undefined) {
-    if (props.type === 1) {
-      return (
-        <div style={{ height: props.height, width: props.width }}>
-          <Loader1 />
-        </div>
-      );
-    }
-    if (props.type === 2) {
-      return (
-        <div style={{ height: props.height, width: props.width }}>
-          <Loader2 />
-        </div>
-      );
+  const { data, type, style, name, children } = props;
+
+  if (data === undefined) {
+    switch (type) {
+      case 1:
+        return (
+          <div style={style}>
+            <Loader1 />
+          </div>
+        );
+      case 2:
+        return (
+          <div style={style}>
+            <Loader2 />
+          </div>
+        );
+      default:
+        return (
+          <div style={style}>
+            <Loader1 />
+          </div>
+        );
     }
   }
-  return React.cloneElement(props.children, { [props.name]: props.data });
+
+  return React.cloneElement(children, { [name]: data });
 };
 
 export default Loader;
--- src/components/weatherWidget/currentWeather/currentMain.jsx
+++ src/components/weatherWidget/currentWeather/currentMain.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import WeatherIcon from "../weatherIcon";
+import Loader from "../../loader";
 
 const CurrentWeatherMain = props => {
   return (
     <div>
       <h3>Current Weather</h3>
-      <WeatherIcon weatherIconNumber={props.weatherIconNumber} />
+      <Loader
+        data={props.weatherIconNumber}
+        name="weatherIconNumber"
+        type={1}
+        height="140px"
+        width="140px"
+      >
+        {/* TODO: I would be great if I can define the size of weather icon as a prop */}
+        <WeatherIcon weatherIconNumber />
+      </Loader>
       <h3>{props.weatherText}</h3>
     </div>
   );
