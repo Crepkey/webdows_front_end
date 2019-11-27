660b9a528af2221a9f2f22d5645976830eadf449
Crepkey <crepkey@gmail.com>
2019-11-27 13:34:31 +0100

    Weather icons handle the style objects

--- src/components/weatherWidget/weatherIcon.jsx
+++ src/components/weatherWidget/weatherIcon.jsx
@@ -37,71 +37,71 @@ const WeatherIcon = props => {
   switch (weatherIconNumber) {
     case 1:
     case 2:
-      return <img src={weather_icon_01} alt="weather_icon_01" />;
+      return <img src={weather_icon_01} style={style} alt="weather_icon_01" />;
     case 3:
     case 4:
-      return <img src={weather_icon_11} alt="weather_icon_11" />;
+      return <img src={weather_icon_11} style={style} alt="weather_icon_11" />;
     case 5:
     case 6:
-      return <img src={weather_icon_12} alt="weather_icon_12" />;
+      return <img src={weather_icon_12} style={style} alt="weather_icon_12" />;
     case 7:
-      return <img src={weather_icon_03} alt="weather_icon_03" />;
+      return <img src={weather_icon_03} style={style} alt="weather_icon_03" />;
     case 8:
-      return <img src={weather_icon_13} alt="weather_icon_13" />;
+      return <img src={weather_icon_13} style={style} alt="weather_icon_13" />;
     case 11:
-      return <img src={weather_icon_08} alt="weather_icon_11" />;
+      return <img src={weather_icon_08} style={style} alt="weather_icon_11" />;
     case 12:
-      return <img src={weather_icon_14} alt="weather_icon_14" />;
+      return <img src={weather_icon_14} style={style} alt="weather_icon_14" />;
     case 13:
     case 14:
-      return <img src={weather_icon_17} alt="weather_icon_17" />;
+      return <img src={weather_icon_17} style={style} alt="weather_icon_17" />;
     case 15:
-      return <img src={weather_icon_04} alt="weather_icon_04" />;
+      return <img src={weather_icon_04} style={style} alt="weather_icon_04" />;
     case 16:
-      return <img src={weather_icon_19} alt="weather_icon_19" />;
+      return <img src={weather_icon_19} style={style} alt="weather_icon_19" />;
     case 17:
-      return <img src={weather_icon_20} alt="weather_icon_20" />;
+      return <img src={weather_icon_20} style={style} alt="weather_icon_20" />;
     case 18:
-      return <img src={weather_icon_15} alt="weather_icon_15" />;
+      return <img src={weather_icon_15} style={style} alt="weather_icon_15" />;
     case 19:
     case 21:
     case 32:
-      return <img src={weather_icon_05} alt="weather_icon_05" />;
+      return <img src={weather_icon_05} style={style} alt="weather_icon_05" />;
     case 20:
     case 43:
-      return <img src={weather_icon_23} alt="weather_icon_23" />;
+      return <img src={weather_icon_23} style={style} alt="weather_icon_23" />;
     case 22:
     case 23:
     case 44:
-      return <img src={weather_icon_28} alt="weather_icon_28" />;
+      return <img src={weather_icon_28} style={style} alt="weather_icon_28" />;
     case 24:
-      return <img src={weather_icon_06} alt="weather_icon_06" />;
+      return <img src={weather_icon_06} style={style} alt="weather_icon_06" />;
     case 25:
     case 29:
-      return <img src={weather_icon_29} alt="weather_icon_29" />;
+      return <img src={weather_icon_29} style={style} alt="weather_icon_29" />;
     case 26:
-      return <img src={weather_icon_21} alt="weather_icon_21" />;
+      return <img src={weather_icon_21} style={style} alt="weather_icon_21" />;
     case 30:
-      return <img src={weather_icon_40} alt="weather_icon_40" />;
+      return <img src={weather_icon_40} style={style} alt="weather_icon_40" />;
     case 31:
-      return <img src={weather_icon_44} alt="weather_icon_44" />;
+      return <img src={weather_icon_44} style={style} alt="weather_icon_44" />;
     case 33:
-      return <img src={weather_icon_02} alt="weather_icon_02" />;
+      return <img src={weather_icon_02} style={style} alt="weather_icon_02" />;
     case 34:
     case 35:
     case 36:
-      return <img src={weather_icon_32} alt="weather_icon_32" />;
+      return <img src={weather_icon_32} style={style} alt="weather_icon_32" />;
     case 37:
     case 38:
-      return <img src={weather_icon_33} alt="weather_icon_33" />;
+      return <img src={weather_icon_33} style={style} alt="weather_icon_33" />;
     case 39:
     case 40:
-      return <img src={weather_icon_35} alt="weather_icon_35" />;
+      return <img src={weather_icon_35} style={style} alt="weather_icon_35" />;
     case 41:
     case 42:
-      return <img src={weather_icon_34} alt="weather_icon_34" />;
+      return <img src={weather_icon_34} style={style} alt="weather_icon_34" />;
     default:
-      return <img src={weather_icon_0} alt="weather_icon_0" />;
+      return <img src={weather_icon_0} style={style} alt="weather_icon_0" />;
   }
 };
 
