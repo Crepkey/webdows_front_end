24870ce41d7ac47aa53d848ba457c9064ef7a536
Crepkey <crepkey@gmail.com>
2019-12-02 13:33:59 +0100

    Loading feature is implented at forecasts section

--- src/components/weatherWidget/weatherForecasts/weatherForecasts.jsx
+++ src/components/weatherWidget/weatherForecasts/weatherForecasts.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import ADayOfForecasts from "./aDayOfForecasts";
-
+import Loader from "../../loader";
+/* TODO: Here I must solve the unique key problem */
 const WeatherForecasts = props => {
   return (
     <div className="weather-forecasts">
-      {props.weatherForecasts.DailyForecasts !== undefined &&
-        props.weatherForecasts.DailyForecasts.map(day => (
-          <ADayOfForecasts key={day.Date} detailsOfTheDay={day} />
-        ))}
+      {(
+        props.weatherForecasts.DailyForecasts || [
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined
+        ]
+      ).map(day => (
+        <Loader
+          type={1}
+          data={day}
+          name="detailsOfTheDay"
+          style={{
+            border: "solid",
+            borderRadius: "25px",
+            borderColor: "#18232e",
+            margin: "5px",
+            padding: "5px",
+            width: "128px",
+            height: "323px",
+            lineHeight: "441px",
+            backgroundColor: "#18232e"
+          }}
+        >
+          <ADayOfForecasts />
+        </Loader>
+      ))}
     </div>
   );
 };
