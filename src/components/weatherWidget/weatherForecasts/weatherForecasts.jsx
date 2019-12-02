24c9da1c264097b6133773197472553f907f9a7a
Crepkey <crepkey@gmail.com>
2019-12-02 14:04:25 +0100

    Animation style is moved to a variable

--- src/components/weatherWidget/weatherForecasts/weatherForecasts.jsx
+++ src/components/weatherWidget/weatherForecasts/weatherForecasts.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ADayOfForecasts from "./aDayOfForecasts";
 import Loader from "../../loader";
-/* TODO: Here I must solve the unique key problem */
+
 const WeatherForecasts = props => {
+  const loadingAnimStyle = {
+    border: "solid",
+    borderRadius: "25px",
+    borderColor: "#18232e",
+    margin: "5px",
+    padding: "5px",
+    width: "128px",
+    height: "323px",
+    lineHeight: "441px",
+    backgroundColor: "#18232e"
+  };
+
   return (
     <div className="weather-forecasts">
       {(
@@ -14,17 +26,7 @@ const WeatherForecasts = props => {
           type={1}
           data={day}
           name="detailsOfTheDay"
-          style={{
-            border: "solid",
-            borderRadius: "25px",
-            borderColor: "#18232e",
-            margin: "5px",
-            padding: "5px",
-            width: "128px",
-            height: "323px",
-            lineHeight: "441px",
-            backgroundColor: "#18232e"
-          }}
+          style={loadingAnimStyle}
         >
           <ADayOfForecasts />
         </Loader>
