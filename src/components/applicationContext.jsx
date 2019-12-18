bc8b74074051e2edd80687c53c2500baccb44ecf
Crepkey <crepkey@gmail.com>
2019-12-18 15:30:54 +0100

    Application closing function init version  is done

--- src/components/applicationContext.jsx
+++ src/components/applicationContext.jsx
@@ -12,7 +12,7 @@ export const ApplicationProvider = props => {
   const [activeApplications, setActiveApplications] = useState([]);
 
   const startAnApp = appName => {
-    console.log("Ide eljut");
+    /* TODO: I need a check here to avoid the simultaneous running of apps */
     const currentActiveApplications = [...activeApplications];
     for (let app of applications) {
       if (app.type.name === appName) {
@@ -20,6 +20,19 @@ export const ApplicationProvider = props => {
         break;
       }
     }
+    setActiveApplications(currentActiveApplications);
+  };
+
+  /* REFACTOR: Maybe it could be better if I used the app component instead of the string which is the parameter currently */
+  const closeAnApp = appName => {
+    const currentActiveApplications = [...activeApplications];
+    let index;
+    for (let app of currentActiveApplications) {
+      if (app.type.name === appName) {
+        break;
+      } else index++;
+    }
+    currentActiveApplications.splice(index, 1);
     setActiveApplications(currentActiveApplications);
   };
 
