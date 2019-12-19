5d66114bb7b7ad3d5e6c37f1ffe01f1fd2ee123c
Crepkey <crepkey@gmail.com>
2019-12-19 14:03:32 +0100

    Init version of func. which sets the app to the top

--- src/components/applicationContext.jsx
+++ src/components/applicationContext.jsx
@@ -5,11 +5,15 @@ export const ApplicationContext = createContext();
 
 export const ApplicationProvider = props => {
   const [activeApplications, setActiveApplications] = useState([]);
+  const orderOfApps = {}; /* This obj is stores the order of applications in the app's layer hierarchy */
 
   const startAnApp = app => {
     if (activeApplications.find(actApp => actApp.type.name === app.type.name)) {
       return alert("This application is already running");
-    } /* TODO: It could be a nicer solution if I use a general modal for error messages */
+    }
+
+    setAppOnTheTop(app.type.name);
+
     const currentActiveApplications = [...activeApplications];
     currentActiveApplications.push(app);
     setActiveApplications(currentActiveApplications);
@@ -20,6 +24,17 @@ export const ApplicationProvider = props => {
     const index = currentActiveApplications.indexOf(app);
     currentActiveApplications.splice(index, 1);
     setActiveApplications(currentActiveApplications);
+  };
+
+  const setAppOnTheTop = appName => {
+    if (Object.keys(orderOfApps).length === 0) {
+      orderOfApps[appName] = 1;
+      return;
+    }
+    for (let key in orderOfApps) {
+      orderOfApps[key] = 0;
+    }
+    orderOfApps[appName] = 1;
   };
 
   return (
@@ -34,3 +49,5 @@ export const ApplicationProvider = props => {
     </ApplicationContext.Provider>
   );
 };
+
+/* TODO: It could be a nicer solution if I use a general modal for error messages */
