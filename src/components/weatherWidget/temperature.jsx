a1776281166937e90ada3c9e84960f44ad2d2eb6
Crepkey <crepkey@gmail.com>
2020-01-07 12:28:34 +0100

    Temp comp is refactored

--- src/components/weatherWidget/temperature.jsx
+++ src/components/weatherWidget/temperature.jsx
@@ -4,52 +4,28 @@ import React from "react";
 /* Styles */
 import Styled from "styled-components";
 
-const HighTemp = Styled.div`
-  color: #ed5a54;
+const Temp = Styled.div`
+  color: ${props => {
+    switch (props.color) {
+      case "red":
+        return "#ed5a54";
+      case "blue":
+        return "#3f88c5";
+      default:
+        return "white";
+    }
+  }}
   font-weight: bold;
-  font-size: 120%;
-  margin: 19.2px 0px 19.2px 0px;
-`;
-
-const LowTemp = Styled.div`
-  color: #3f88c5;
-  font-weight: bold;
-  font-size: 120%;
-  margin: 19.2px 0px 19.2px 0px;
-`;
-
-const LargeTemp = Styled.div`
-  color: white;
-  font-size: 300%;
-  font-weight: bold;
-  margin: 0px 0px 20px 0px;
+  font-size: ${props => (props.size === "large" ? "300%" : "120%")};
+  margin: 19.2px 0px 20px 0px;
 `;
 
 const Temperature = props => {
-  if (props.color === "blue") {
-    return (
-      <LowTemp>
-        {props.value} {props.unit === "C" ? "°C" : "°F"}
-      </LowTemp>
-    );
-  } else if (props.color === "red") {
-    return (
-      <HighTemp>
-        {props.value} {props.unit === "C" ? "°C" : "°F"}
-      </HighTemp>
-    );
-  }
-  if (props.size === "large") {
-    return (
-      <LargeTemp>
-        {props.value} {props.unit === "C" ? "°C" : "°F"}
-      </LargeTemp>
-    );
-  }
+  return (
+    <Temp color={props.color} size={props.size}>
+      {props.value} {props.unit === "C" ? "°C" : "°F"}
+    </Temp>
+  );
 };
 
 export default Temperature;
-
-/* TODO: Here I can apply only one styled-components which calculating its own CSS attributes based on
-the incoming props
-*/
