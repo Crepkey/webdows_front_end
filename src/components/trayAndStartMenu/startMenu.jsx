159db6c93b1f8d81b429ac917be975f749fb4f9a
Crepkey <crepkey@gmail.com>
2020-01-08 17:10:18 +0100

    Basic start menu with animation is done

--- src/components/trayAndStartMenu/startMenu.jsx
+++ src/components/trayAndStartMenu/startMenu.jsx
@@ -2,23 +2,64 @@
 import React from "react";
 
 /* Styles */
-import Styled from "styled-components";
+import Styled, { keyframes } from "styled-components";
+
+const OpenAnimationForControlBar = keyframes`
+    0% {
+      height: 0px;
+      opacity: 0.5;
+    }
+    100% {
+      height: 600px;
+      opacity: 0.6;
+    } 
+`;
+
+const OpenAnimationForMainContainer = keyframes`
+    50% {
+      width: 0px;
+      opacity: 0.5;
+    }
+    100% {
+      width: 400px;
+      opacity: 0.6;
+    } 
+`;
 
 const MainContainer = Styled.div`
-    background: #005a9e;
-    position: absolute;
-    left: 0;
-    bottom: 43px;
-    height: 600px;
-    color: white;
-    visibility: hidden;
-    display: grid;
-    grid-template-columns: 50px 250px 400px;
-    cursor: default;
+  position: absolute;
+  left: 0px;
+  bottom: 43px;
+  height: 600px;
+  z-index: 999999;
+  cursor: default;
+  color: white;
+  background: #000000;
+  animation-name: ${OpenAnimationForMainContainer}
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+`;
+
+const ControlBar = Styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 43px;
+  z-index: 999999;
+  width: 64px;
+  color: white;
+  background: #000000;
+  animation-name: ${OpenAnimationForControlBar}
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
 `;
 
 const StartMenu = () => {
-  return <MainContainer>TESZT</MainContainer>;
+  return (
+    <React.Fragment>
+      <ControlBar />
+      <MainContainer />
+    </React.Fragment>
+  );
 };
 
 export default StartMenu;
