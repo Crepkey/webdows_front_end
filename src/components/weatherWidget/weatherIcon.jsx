7c4b984cb67c6742c0ca9986450427dc03ad8676
Crepkey <crepkey@gmail.com>
2020-01-17 14:08:38 +0100

    react-create-app on Heroku

--- src/components/weatherWidget/weatherIcon.jsx
+++ src/components/weatherWidget/weatherIcon.jsx
@@ -7,6 +7,7 @@ const WeatherIcon = props => {
   function importAll(r) {
     return r.keys().map(r);
   }
+  
   const images = importAll(
     require.context("../../img", false, /\.(png|jpe?g|svg)$/)
   );
