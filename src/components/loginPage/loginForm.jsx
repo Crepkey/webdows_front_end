6e784480b62a6affb126f4affb0c56d069994264
Crepkey <crepkey@gmail.com>
2020-02-10 17:15:32 +0100

    LoginFrom is created, Login moved to trash

--- src/components/loginPage/loginForm.jsx
+++ src/components/loginPage/loginForm.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+/* Comps */
+import Input from "../input";
+
+/* Styles */
+import Styled from "styled-components";
+
+const Form = Styled.form`
+  border: solid 1px white;
+  height: 100vh;
+  width: 50%;
+`;
+
 const LoginForm = props => {
-  return <h1 style={{ color: "white" }}>Login Form</h1>;
+  return (
+    <React.Fragment>
+      <Form>
+        <h1 style={{ color: "white" }}>Login Form</h1>
+        <Input
+          id="username"
+          name="username"
+          htmlFor="username"
+          label="Username"
+        ></Input>
+        <Input
+          id="password"
+          name="password"
+          htmlFor="username"
+          label="Password"
+          type="password"
+        />
+        <button type="submit">Login</button>
+      </Form>
+    </React.Fragment>
+  );
 };
 
 export default LoginForm;
